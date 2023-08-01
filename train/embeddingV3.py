a73b396ba1326424b8bae80d494094276d6b16b3
李茂益 <limaoyi@tianhecloud.com>
2023-08-01 16:09:16 +0800

    优化文档质量,去掉无关信息

--- train/embeddingV3.py
+++ train/embeddingV3.py
@@ -48,7 +48,7 @@ docs = docs1
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size =300,
+    chunk_size =1000,
     chunk_overlap = 100,
     length_function = len,
 )
