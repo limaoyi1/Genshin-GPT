94f27d87273bd18dff2063849a2c3a8608f5b2a6
李茂益 <limaoyi@tianhecloud.com>
2023-08-02 13:45:44 +0800

    添加温度

--- chain/gpt_memory.py
+++ chain/gpt_memory.py
@@ -50,7 +50,7 @@ This is your Conversation with '旅行者':
             input_variables=["chat_history", "human_input"], template=self.template)
         llm_chain = LLMChain(
             llm=OpenAI(model_name="gpt-3.5-turbo-16k", openai_api_key=self.openai_api_key,
-                       openai_api_base=self.openai_base_url, streaming=True,
+                       openai_api_base=self.openai_base_url, streaming=True, temperature=0.7,
                        callbacks=[StreamingStdOutCallbackHandler()]),
             prompt=prompt,
             verbose=True,
